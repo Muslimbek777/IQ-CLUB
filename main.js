5cba87bebb82fdb0a38ab327baac3532bab18823
Muslimbek777 <muborakradzapova1@gmail.com>
2026-02-22 12:12:03 +0500

    full

--- main.js
+++
@@ -1,7 +0,0 @@
-const headerBtn = document.getElementById("header-btn");
-const navbar = document.getElementById("navbar");
-
-headerBtn.addEventListener("click", () => {
-  navbar.classList.toggle("open");
-  headerBtn.classList.toggle("close-btn");
-});
